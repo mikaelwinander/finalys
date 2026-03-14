61f0906245645af70b729c8a0ae428a8f6f34bd2
mikael.winander <mikael.winander@visma.com>
2026-03-14 22:50:32 +0000

    fix(frontend): update tailwind css configuration to v4

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,7 +5,7 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_20
+    pkgs.nodejs_22
   ];
   # Sets environment variables in the workspace
   env = {};
@@ -18,9 +18,9 @@
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-        npm-install = "npm i --no-audit --no-progress --timing";
+        npm-install = "cd finalys-app/frontend && npm i --no-audit --no-progress --timing";
         # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "src/App.tsx" "src/App.ts" "src/App.jsx" "src/App.js" ];
+        default.openFiles = [ "finalys-app/frontend/src/App.tsx" "finalys-app/frontend/src/App.ts" "finalys-app/frontend/src/App.jsx" "finalys-app/frontend/src/App.js" ];
       };
       # To run something each time the workspace is (re)started, use the `onStart` hook
     };
@@ -30,6 +30,7 @@
       previews = {
         web = {
           command = ["npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0"];
+          cwd = "finalys-app/frontend";
           manager = "web";
         };
       };
